e2a64eb6e799a9cae6de540eb0b72acf1236fcb8
Martin Turjak <martinturjak@gmail.com>
2015-02-07 01:11:48 +0100

    added facebook sdk and integrated facebook login gutton into login activity

--- app/src/main/java/com/newtpond/testnavdrawer/LoginActivity.java
+++ app/src/main/java/com/newtpond/testnavdrawer/LoginActivity.java
@@ -10,10 +10,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends Activity {
 
@@ -21,6 +31,7 @@ public class LoginActivity extends Activity {
     protected EditText mUsername;
     protected EditText mPassword;
     protected Button mLoginButton;
+    protected Button mFbLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,7 @@ public class LoginActivity extends Activity {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
 
+        // sign up link
         mSignUpTextView = (TextView)findViewById(R.id.signupText);
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +55,7 @@ public class LoginActivity extends Activity {
         mUsername = (EditText)findViewById(R.id.usernameField);
         mPassword = (EditText)findViewById(R.id.passwordField);
         mLoginButton = (Button)findViewById(R.id.loginButton);
+        mFbLoginButton = (Button)findViewById(R.id.fbLoginButton);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,10 +82,8 @@ public class LoginActivity extends Activity {
                             // setSupportProgressBarIndeterminateVisibility(false);
 
                             if (user != null) {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
+                                // start main activity
+                                finishActivity();
                             } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                 builder.setMessage(e.getMessage())
@@ -86,11 +97,88 @@ public class LoginActivity extends Activity {
                 }
             }
         });
+
+        mFbLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onFbLoginButtonClicked();
+            }
+        });
+    }
+
+    private void finishActivity() {
+        // Start an intent for the dispatch activity
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         return true;
+    }
+
+    /**
+     * Facebook login related methods
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
+
+    private void onFbLoginButtonClicked() {
+        List<String> permissions = Arrays.asList("public_profile");
+        ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+                if (user != null) {
+                    if (user.isNew()) {
+                        // set favorites as null, or mark it as empty somehow
+                        makeMeRequest();
+                    } else {
+                        finishActivity();
+                    }
+                }
+            }
+        });
+    }
+
+    private void makeMeRequest() {
+        Session session = ParseFacebookUtils.getSession();
+        if (session != null && session.isOpened()) {
+            Request request = Request.newMeRequest(
+                    ParseFacebookUtils.getSession(),
+                    new Request.GraphUserCallback() {
+                        @Override
+                        public void onCompleted(GraphUser user,
+                                                Response response) {
+                            if (user != null) {
+                                ParseUser.getCurrentUser().put("firstName",
+                                        user.getFirstName());
+                                ParseUser.getCurrentUser().saveInBackground();
+                                finishActivity();
+                            } else if (response.getError() != null) {
+                                if ((response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_RETRY)
+                                        || (response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_REOPEN_SESSION)) {
+                                    // TODO: make dialog error instead of toast
+                                    Toast.makeText(getApplicationContext(),
+                                            R.string.session_invalid_error,
+                                            Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    // TODO: make dialog error instead of toast
+                                    Toast.makeText(getApplicationContext(),
+                                            R.string.logn_generic_error,
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+            request.executeAsync();
+
+        }
     }
 }
--- app/src/main/java/com/newtpond/testnavdrawer/TestNavDrawerApplication.java
+++ app/src/main/java/com/newtpond/testnavdrawer/TestNavDrawerApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 //import com.deploygate.sdk.DeployGate;
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.PushService;
 
 /**
  * TestNavDrawer
@@ -14,7 +16,15 @@ public class TestNavDrawerApplication extends Application {
         super.onCreate();
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "Nasr5RWhl5eyrYY5RnWcgDaJq5kU4QRbHlZwy95i", "hq6Da4CNp6yVRzt9F08krQfstMaYWnrMLU0DQIhQ");
+        Parse.initialize(this, getString(R.string.parse_application_id), getString(R.string.parse_client_key));
+
+        // Initialize Facebook
+        String appId = getString(R.string.facebook_app_id);
+        ParseFacebookUtils.initialize(appId);
+
+        // Set up Push TODO
+        //PushService.setDefaultPushCallback(this, AlertsActivity.class);
+        //ParseInstallation.getCurrentInstallation().saveInBackground();
 
         // DeployGate.install(this);
     }
