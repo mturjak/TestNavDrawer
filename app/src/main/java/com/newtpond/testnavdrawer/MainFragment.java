6fd6e596f92f55e4d2de27f7dd8dba5af110352b
Martin Turjak <martinturjak@gmail.com>
2015-02-10 17:59:02 +0100

    added MainFragment class and dummy content

---
+++ app/src/main/java/com/newtpond/testnavdrawer/MainFragment.java
@@ -0,0 +1,58 @@
+package com.newtpond.testnavdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.newtpond.testnavdrawer.dummy.DummyContent;
+
+/**
+ * Main fragment class
+ */
+public class MainFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MainFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.section_label)).setText(mItem.description);
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/newtpond/testnavdrawer/dummy/DummyContent.java
@@ -0,0 +1,59 @@
+package com.newtpond.testnavdrawer.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p/>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new DummyItem("1", "The Beginner's Guide to Android", "Description 1"));
+        addItem(new DummyItem("2", "Why Developing Apps for Android is Important", "Description 2"));
+        addItem(new DummyItem("3", "Why Developing Apps for Android is Fun", "Description 3"));
+        addItem(new DummyItem("4", "Who Wins the Fight Between Android and iOS in 2012?", "Description 4"));
+        addItem(new DummyItem("5", "Java Basics for Android Development - Part 1", "Description 5"));
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public String id;
+        public String content;
+        public String description;
+
+        public DummyItem(String id, String content, String description) {
+            this.id = id;
+            this.content = content;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
